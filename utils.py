2a83f0ee756e450407fe2fa0eed17f8bbf7203b7
Will Luer <wluer@wustl.edu>
2019-11-11 20:45:19 -0500

    added function used in all_wordifications, renamed function to be more generic

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ def clean_input(input):
     return cleaned
 
 # Creates dictionary of {char: number,}
-def construct_char_map(digit_map):
+def reverse_dict(digit_map):
     char_map = {}
     for digit,chars in digit_map.items():
         for char in chars:
@@ -53,3 +53,30 @@ def is_valid_input(input,regex):
         print("Input phone number must have 10 or 11 digits")
         return False
     return True
+
+#https://stackoverflow.com/questions/2556108/rreplace-how-to-replace-the-last-occurrence-of-an-expression-in-a-string
+def rreplace(s,old,new,occurence):
+    li = s.rsplit(old,occurence)
+    return new.join(li)
+
+
+# https://stackoverflow.com/questions/35091557/replace-nth-occurrence-of-substring-in-string
+# Replace nth occurence of a substring
+# Used when multiple instances of a digit, e.g. '96' repeats in 96196
+def nth_substr_repl(s, sub, repl, nth):
+    # print("s {}".format(s))
+    # print("sub {}".format(sub))
+    # print("repl {}".format(repl))
+    # print("nth {}".format(nth))
+    find = s.find(sub)
+    # if find is not p1 we have found at least one match for the substring
+    i = find != -1
+    # loop util we find the nth or we find no match
+    while find != -1 and i != nth:
+        # find + 1 means we start at the last match start index + 1
+        find = s.find(sub, find + 1)
+        i += 1
+    # if i  is equal to nth we found nth matches so replace
+    if i == nth:
+        return s[:find]+repl+s[find + len(sub):]
+    return s
